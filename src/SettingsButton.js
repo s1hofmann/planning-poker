e70d276f8e221d32a2f4485e5e86d6cd1979a75e
Simon Hofmann <mail@simon-hofmann.org>
2019-12-10 12:48:47 +0100

    (#9) Updated SettingsButton to respect theme data

--- src/SettingsButton.js
+++ src/SettingsButton.js
@@ -2,11 +2,12 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledButton = styled.span`
-  color: ${props => props.theme.color};
+  color: ${props => props.theme.topbar.color};
   text-align: right;
   padding-right: 10px;
   text-shadow: 1px 1px lightgray;
   font-size: 1em;
+  cursor: pointer;
 `;
 
 export const SettingsButton = props => (
