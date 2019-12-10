5e11430c24b1fe85dcb819d7b0dfaf501a795fb6
s1hofmann <s1hofmann@users.noreply.github.com>
2019-12-10 12:10:04 +0000

    Deploying to gh-pages from master 10c1040ec5520a1ab7682512d5d567a846284930

---
+++ precache-manifest.8e8cd11d8afc6266d202d429e750c802.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ef0ad8e5847a2de9fa7d554c88bf8e4",
+    "url": "/planning-poker/index.html"
+  },
+  {
+    "revision": "db4594264c30cabc8aec",
+    "url": "/planning-poker/static/js/2.d5ad53ed.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/planning-poker/static/js/2.d5ad53ed.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0dd840dab08f3d522565",
+    "url": "/planning-poker/static/js/main.ef6efde7.chunk.js"
+  },
+  {
+    "revision": "680117373cbdbce8f36d",
+    "url": "/planning-poker/static/js/runtime-main.9aa9215e.js"
+  }
+]);
